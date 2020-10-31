8e7e252e4768e9c36aba8e7f5bc16bce2d46b501
Jefferson Gouveia <jeffersongouveia@eucatur.com.br>
2020-10-31 18:38:48 -0400

    ⌚️ Appointments header and card of the next

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import { FiPower } from 'react-icons/all'
+import { FiClock, FiPower } from 'react-icons/all'
 
 import { useAuth } from '../../hooks/auth'
 
-import { Container, Header, HeaderContent, Profile } from './styles'
+import { Calendar, Container, Content, Header, HeaderContent, NextAppointment, Profile, Schedule } from './styles'
 import logo from '../../assets/logo.svg'
 
 const Dashboard: React.FC = () => {
@@ -29,6 +29,34 @@ const Dashboard: React.FC = () => {
           </button>
         </HeaderContent>
       </Header>
+
+      <Content>
+        <Schedule>
+          <h1>Appointments</h1>
+
+          <p>
+            <span>Today</span>
+            <span>Day 3</span>
+            <span>Monday</span>
+          </p>
+
+          <NextAppointment>
+            <strong>Next appointment</strong>
+
+            <div>
+              <img src="https://pbs.twimg.com/profile_images/1319309850887806976/hAaOYHJB_400x400.jpg" alt="Mell Pereira" />
+
+              <strong>Mell Pereira</strong>
+              <span>
+                <FiClock />
+                08:00
+              </span>
+            </div>
+          </NextAppointment>
+        </Schedule>
+
+        <Calendar />
+      </Content>
     </Container>
   )
 }
