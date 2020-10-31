4890c41d1a16ee0de0bdef4a664e4f6df41af344
Jefferson Gouveia <jeffersongouveia@eucatur.com.br>
2020-10-31 14:27:43 -0400

    👱🏾‍♀️ Header for Dashboard

--- src/pages/Dashboard/index.tsx
+++ src/pages/Dashboard/index.tsx
@@ -1,10 +1,35 @@
 import React from 'react'
+import { FiPower } from 'react-icons/all'
+
+import { useAuth } from '../../hooks/auth'
+
+import { Container, Header, HeaderContent, Profile } from './styles'
+import logo from '../../assets/logo.svg'
 
 const Dashboard: React.FC = () => {
+  const { user, signOut } = useAuth()
+
   return (
-    <>
-      <h1>Dashboard</h1>
-    </>
+    <Container>
+      <Header>
+        <HeaderContent>
+          <img src={logo} alt="GoBarber" />
+
+          <Profile>
+            <img src="https://avatars2.githubusercontent.com/u/12664845?s=460&u=281dbbda2311796bc08b3529a069a39ed076f222&v=4" alt={user.name} />
+
+            <div>
+              <span>Welcome</span>
+              <strong>Jeff Gouveia</strong>
+            </div>
+          </Profile>
+
+          <button type="button" onClick={signOut}>
+            <FiPower />
+          </button>
+        </HeaderContent>
+      </Header>
+    </Container>
   )
 }
 
